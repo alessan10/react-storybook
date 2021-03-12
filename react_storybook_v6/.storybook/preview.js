516c26d2ad5184af6b0208880719b4ba5b4d8941
alessandromauro.m@outlook.com <alex.mauro@live.it>
2021-03-12 12:44:30 +0100

    added Chakra

--- react_storybook_v6/.storybook/preview.js
+++ react_storybook_v6/.storybook/preview.js
@@ -1,4 +1,6 @@
 import React from 'react'
+//import Center from '../src/components/Center/Center'
+import { ThemeProvider, theme, CSSReset, Box} from '@chakra-ui/react'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -7,3 +9,14 @@ export const parameters = {
       a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
   },
 }
+
+export const decorators = [
+  (Story) => (
+      <ThemeProvider theme={theme}>
+        <CSSReset />
+        <Box m='4'>
+         <Story /> 
+        </Box>
+      </ThemeProvider>
+    )
+]
