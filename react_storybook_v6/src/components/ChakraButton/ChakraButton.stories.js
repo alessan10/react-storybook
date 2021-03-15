3275380baa4c15d15cd800c56ed4161cce92783b
alessandromauro.m@outlook.com <alex.mauro@live.it>
2021-03-15 12:14:59 +0100

    ended storybook series - problems with chakra

--- react_storybook_v6/src/components/ChakraButton/ChakraButton.stories.js
+++ react_storybook_v6/src/components/ChakraButton/ChakraButton.stories.js
@@ -1,10 +1,38 @@
 import React from 'react'
 import {Button} from '@chakra-ui/react'
+//import { Button} from '@chakra-ui/core'
 
 export default {
     title: 'Chakra/Button',
-    component: Button
+    component: Button,
+    argTypes: {
+        variantColor: {control: 'text'},
+        children: {control: 'text'},
+        onClick: {action: 'clicked'}
+    }
 }
 
-export const Success = () => <Button variantColor='green'>Success</Button>
-export const Danger = () => <Button variantColor='red'>Danger</Button>
+const Template = args => <Button {...args}/>
+
+export const Success = Template.bind({})
+Success.args = {
+    variantColor: 'green',
+    children: 'Success'
+}
+
+export const Danger = Template.bind({})
+Danger.args = {
+    variantColor: 'red',
+    children: 'Danger'
+}
+
+export const Log = () => (
+    <Button variantColor='blue' onClick={() => console.log('Button Clicked', process.env.STORYBOOK_THEME)}>
+        Log
+    </Button>
+)
+
+/*
+export const Success = () => <Button bgColor='red' size="l">Success</Button>
+export const Danger = () => <Button colorScheme="red" size="xs">Danger</Button>
+*/
